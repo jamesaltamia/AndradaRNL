f3e23e38dba19bca80cbd0ea798793e239c22a30
jameskie123 <racklyhero23@gmail.com>
2025-04-07 21:13:30 +0800

    LoadUsers Finished

--- client/src/services/UserService.ts
+++ client/src/services/UserService.ts
@@ -1,8 +1,16 @@
 import AxiosInstance from "../AxiosInstance";
 
 const UserService = {
+    loadUser: async () => {
+        return AxiosInstance.get("/loadUsers")
+        .then((response) => response)
+        .catch((error) => {
+            throw error;
+        });
+    },
+
     storeUser: async (data: any) => {
-        return AxiosInstance.post('/storeUser', data)
+        return AxiosInstance.post("/storeUser", data)
         .then((response) => response)
         .catch((error) => {
             throw error;
--- server/routes/api.php
+++ server/routes/api.php
@@ -15,6 +15,7 @@ Route::controller(GenderController::class)->group(function () {
 
 
 Route::controller(UserController::class)->group(function () {
+    Route::get('/loadUsers', 'loadUsers');
     Route::post('/storeUser', 'storeUser');
 });
 // Route::get('/user', function (Request $request) {
